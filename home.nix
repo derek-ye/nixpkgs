cdd3f9d55196a6e0d1795586c12c870347c985d5
Derek <derek.ye@freckle.com>
2023-11-10 16:24:29 -0800

    Initial commit following Ju Liu's guide - set up nix with flakes and home-manager

---
+++ home.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }: {
+  home.username = "derek.ye"; # REPLACE ME
+  home.homeDirectory = "/Users/derek.ye"; # REPLACE ME
+  home.stateVersion = "22.11";
+  programs.home-manager.enable = true;
+}
